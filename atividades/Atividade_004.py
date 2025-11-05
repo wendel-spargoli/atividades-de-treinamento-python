8d164b05f6cfaa5ad98fa768ad75f1a885465142
Wendel Spargoli <wendelspargolicontato@gmail.com>
2025-11-05 16:01:48 -0300

    Add files via upload

---
+++ atividades/Atividade_004.py
@@ -0,0 +1,12 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+# Notas dos bimestres
+bimestre_1 = float(input("Digite a nota que você tirou: "))
+bimestre_2 = float(input("Digite a nota que você tirou: "))
+bimestre_3 = float(input("Digite a nota que você tirou: "))
+bimestre_4 = float(input("Digite a nota que você tirou: "))
+
+# Média
+media = (bimestre_1 + bimestre_2 + bimestre_3 + bimestre_4) / 4
+
+print(media)
